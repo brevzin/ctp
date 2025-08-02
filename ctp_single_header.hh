6b946d6179655a436a0730ab7e573fc892268c70
Barry Revzin <barry.revzin@gmail.com>
2025-08-02 06:30:00 -0500

    Adding std::meta::is_structural

--- ctp_single_header.hh
+++ ctp_single_header.hh
@@ -10,29 +10,54 @@
 namespace ctp {
 
 // The customization point to opt-in to having your type be usable as a
-// ctp::Param. Reflect<T> can take one of two forms.
+// ctp::Param.
 //
-// Either:
+// Reflect<T> must provide a target_type, a serialize() with the
+// following signature, and one of three forms of deserialization function:
+//
 //
 //  struct {
 //      using target_type = ????;
 //      static consteval auto serialize(Serializer&, T const&) -> void;
+//
+//      // Option 1. Take all of the serialized infos as constant template parameters
+//      template <std::meta::info... Is>
+//      static consteval auto deserialize() -> target_type;
+//
+//      // Option 2: Take all of the serialized infos as function parameters
 //      static consteval auto deserialize(std::meta::info...) -> target_type;
+//
+//      // Option 3: Take the splices of all of the serialized infos as function
+//      // parameters
+//      static consteval auto deserialize_constants(auto&&...) -> target_type;
+//
 //  };
-//
-// 1. The target_type need not be C++20-structural itself, but cannot allocate.
-// 2. serialize() can push any number of std::meta::info's into the serializer.
-// 3. deserializer() will be invoked with the std::meta::info's that were
-//    pushed.
-//
-// Or:
-//
-//  struct {
-//      using target_type = ????;
-//      static consteval auto serialize(T const&) -> std::meta::info;
-//  };
-//
-// Where serialize() has to return a reflection of an object of type target_type.
+
+#ifndef CTP_META_IS_STRUCTURAL
+namespace std::meta {
+    // this really should be in std:: but it's not yet, so...
+    consteval auto is_structural_type(info type) -> bool {
+        auto ctx = std::meta::access_context::unchecked();
+
+        return is_scalar_type(type)
+            or is_lvalue_reference_type(type)
+            or is_class_type(type)
+                and std::ranges::all_of(bases_of(type, ctx),
+                        [](std::meta::info o){
+                            return is_public(o)
+                            and is_structural_type(type_of(o));
+                        })
+                and std::ranges::all_of(nonstatic_data_members_of(type, ctx),
+                        [](std::meta::info o){
+                            return is_public(o)
+                            and not is_mutable_member(o)
+                            and is_structural_type(
+                                    remove_all_extents(type_of(o)));
+                        });
+
+    }
+}
+#endif
 
 template <class T>
 struct Reflect;
@@ -52,23 +77,19 @@ template <class T>
 using target_or_ref = [: is_lvalue_reference_type(^^T) ? ^^T : substitute(^^target, {^^T}) :];
 
 
-// extract<T>(r) works for values and objects (but copies) while
-// extract<T const&>(r) works only for objects.
-// This ensures that we do the efficient thing.
-template <class T>
-inline constexpr auto extract_maybe_ref = [](std::meta::info r) -> decltype(auto) {
-    if constexpr (is_class_type(^^T)) {
-        return extract<T const&>(r);
-    } else {
-        return extract<T>(r);
-    }
-};
-
 namespace impl {
     // This is the singular (private) object that will be
     // constructed from the serialization-deserialization round trip.
     template <class T, std::meta::info... Is>
-    inline constexpr target<T> the_object = Reflect<T>::deserialize(Is...);
+    inline constexpr target<T> the_object = []{
+        if constexpr (requires { Reflect<T>::template deserialize<Is...>(); }) {
+            return Reflect<T>::template deserialize<Is...>();
+        } else if constexpr (requires { Reflect<T>::deserialize(Is...); }) {
+            return Reflect<T>::deserialize(Is...);
+        } else {
+            return Reflect<T>::deserialize_constants([:Is:]...);
+        }
+    }();
 
     // This is the singular (private) object that will be used for reflect_constant_array
     template <class T, std::meta::info... Is>
@@ -79,20 +100,29 @@ namespace impl {
     consteval auto default_serialize(T const& v) -> std::meta::info;
 }
 
+inline constexpr auto normalize =
+    []<class T>([[maybe_unused]] T& v) -> void {
+        #ifdef CTP_HAS_STRING_LITERAL
+        if constexpr (requires { std::is_string_literal(v); }) {
+            if (char const* root = std::string_literal_from(v)) {
+                char const* global = std::define_static_string(std::string_view(root));
+                v = global + (v - root);
+            }
+        }
+        #endif
+    };
+
 // Extension of std::meta::reflect_constant that is customizable via Reflect<T>.
 // For scalar types, returns a reflection representing a value.
 // For class types, returns a reflection representing an object.
 inline constexpr auto reflect_constant =
-    []<class T>(T const& v){
+    []<class T>(T v){
         if constexpr (is_structural_type(^^T)) {
+            normalize(v);
             return std::meta::reflect_constant(v);
         } else {
             // For non-structural types, customize via Reflect<T>::serialize
-            if constexpr (requires { Reflect<T>::serialize(v); }) {
-                return Reflect<T>::serialize(v);
-            } else {
-                return impl::default_serialize(v);
-            }
+            return impl::default_serialize(v);
         }
     };
 
@@ -118,19 +148,6 @@ inline constexpr auto define_static_object =
             // should be std::define_static_object but not implemented in clang
             return std::define_static_array(std::span(std::addressof(v), 1))[0];
         }
-    };
-
-
-inline constexpr auto normalize =
-    []<class T>([[maybe_unused]] T& v) -> void {
-        #ifdef CTP_HAS_STRING_LITERAL
-        if constexpr (requires { std::is_string_literal(v); }) {
-            if (char const* root = std::string_literal_from(v)) {
-                char const* global = std::define_static_string(std::string_view(root));
-                v = global + (v - root);
-            }
-        }
-        #endif
     };
 
 }
@@ -214,18 +231,19 @@ namespace ctp {
 // template parameter of type T(~ish).
 template <class T>
 struct Param {
-    using U = target<T>;
-    U const& value;
+    using type = target<T>;
+    type const& value;
 
     consteval Param(T const& v) : value(define_static_object(v)) { }
-    consteval operator U const&() const { return value; }
-    consteval auto get() const -> U const& { return value; }
-    consteval auto operator*() const -> U const& { return value; }
-    consteval auto operator->() const -> U const* { return std::addressof(value); }
+    consteval operator type const&() const { return value; }
+    consteval auto get() const -> type const& { return value; }
+    consteval auto operator*() const -> type const& { return value; }
+    consteval auto operator->() const -> type const* { return std::addressof(value); }
 };
 
 template <class T> requires (is_structural_type(^^T))
 struct Param<T> {
+    using type = T;
     T value;
 
     consteval Param(T const& v) : value(v) { ctp::normalize(value); }
@@ -282,9 +300,9 @@ namespace ctp {
             }
         }
 
-        static consteval auto deserialize() -> target_type { return {}; }
-        static consteval auto deserialize(std::meta::info r) -> target_type {
-            return extract_maybe_ref<target<T>>(r);
+        static consteval auto deserialize_constants() -> target_type { return {}; }
+        static consteval auto deserialize_constants(target_or_ref<T> const& v) -> target_type {
+            return v;
         }
     };
 
@@ -297,8 +315,8 @@ namespace ctp {
             (s.push_constant_or_object(^^decltype(elems), elems), ...);
         }
 
-        static consteval auto deserialize(auto... rs) -> target_type {
-            return target_type(extract_maybe_ref<target_or_ref<Ts>>(rs)...);
+        static consteval auto deserialize_constants(target_or_ref<Ts> const&... vs) -> target_type {
+            return target_type(vs...);
         }
     };
 
@@ -306,24 +324,61 @@ namespace ctp {
     struct Reflect<std::variant<Ts...>> {
         using target_type = std::variant<target<Ts>...>;
 
-        template <size_t I, std::meta::info R>
-        static constexpr auto the_object = target_type(std::in_place_index<I>, [:R:]);
-
-        static consteval auto serialize(std::variant<Ts...> const& v) -> std::meta::info {
+        static consteval auto serialize(Serializer& s, std::variant<Ts...> const& v) -> void {
+            s.push_constant(v.index());
             // visit should work, but can't because of LWG4197
             template for (constexpr size_t I : std::views::iota(0zu, sizeof...(Ts))) {
                 if (I == v.index()) {
-                    return substitute(
-                        ^^the_object,
-                        {
-                            reflect_constant(I),
-                            reflect_constant(reflect_constant(std::get<I>(v)))
-                        }
-                    );
+                    s.push_constant(std::get<I>(v));
+                    return;
                 }
             }
+        }
 
-            std::unreachable();
+        template <std::meta::info I, std::meta::info R>
+        static consteval auto deserialize() -> target_type {
+            return target_type(std::in_place_index<([:I:])>, [:R:]);
+        }
+    };
+
+    template <class T>
+    struct Reflect<std::reference_wrapper<T>> {
+        using target_type = std::reference_wrapper<T>;
+
+        static consteval auto serialize(Serializer& s, std::reference_wrapper<T> r) -> void {
+            s.push_object(r.get());
+        }
+
+        static consteval auto deserialize_constants(T& r) -> target_type {
+            return r;
+        }
+    };
+
+    template <>
+    struct Reflect<std::string_view> {
+        using target_type = std::string_view;
+
+        static consteval auto serialize(Serializer& s, std::string_view sv) -> void {
+            s.push_constant(sv.data());
+            s.push_constant(sv.size());
+        }
+
+        static consteval auto deserialize_constants(char const* data, size_t size) -> std::string_view {
+            return std::string_view(data, size);
+        }
+    };
+
+    template <class T, size_t N>
+    struct Reflect<std::span<T, N>> {
+        using target_type = std::span<T, N>;
+
+        static consteval auto serialize(Serializer& s, std::span<T, N> sp) -> void {
+            s.push_constant(sp.data());
+            s.push_constant(sp.size());
+        }
+
+        static consteval auto deserialize_constants(T const* data, size_t size) -> target_type {
+            return target_type(data, size);
         }
     };
 }
--- include/ctp/core.hh
+++ include/ctp/core.hh
@@ -30,6 +30,32 @@ namespace ctp {
 //
 //  };
 
+#ifndef CTP_META_IS_STRUCTURAL
+namespace std::meta {
+    // this really should be in std:: but it's not yet, so...
+    consteval auto is_structural_type(info type) -> bool {
+        auto ctx = std::meta::access_context::unchecked();
+
+        return is_scalar_type(type)
+            or is_lvalue_reference_type(type)
+            or is_class_type(type)
+                and std::ranges::all_of(bases_of(type, ctx),
+                        [](std::meta::info o){
+                            return is_public(o)
+                            and is_structural_type(type_of(o));
+                        })
+                and std::ranges::all_of(nonstatic_data_members_of(type, ctx),
+                        [](std::meta::info o){
+                            return is_public(o)
+                            and not is_mutable_member(o)
+                            and is_structural_type(
+                                    remove_all_extents(type_of(o)));
+                        });
+
+    }
+}
+#endif
+
 template <class T>
 struct Reflect;
 
