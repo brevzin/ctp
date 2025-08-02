0b453eee89a891f167c30d108771b54e6d5536fd
Barry Revzin <barry.revzin@gmail.com>
2025-08-02 06:35:47 -0500

    lol, wrong namespace

--- include/ctp/core.hh
+++ include/ctp/core.hh
@@ -4,6 +4,8 @@
 #include <meta>
 #include <ranges>
 
+#include <ctp/meta.hh>
+
 namespace ctp {
 
 // The customization point to opt-in to having your type be usable as a
@@ -29,33 +31,6 @@ namespace ctp {
 //      static consteval auto deserialize_constants(auto&&...) -> target_type;
 //
 //  };
-
-#ifndef CTP_META_IS_STRUCTURAL
-#include <algorithm>
-namespace std::meta {
-    // this really should be in std:: but it's not yet, so...
-    consteval auto is_structural_type(info type) -> bool {
-        auto ctx = access_context::unchecked();
-
-        return is_scalar_type(type)
-            or is_lvalue_reference_type(type)
-            or is_class_type(type)
-                and ranges::all_of(bases_of(type, ctx),
-                        [](info o){
-                            return is_public(o)
-                            and is_structural_type(type_of(o));
-                        })
-                and ranges::all_of(nonstatic_data_members_of(type, ctx),
-                        [](info o){
-                            return is_public(o)
-                            and not is_mutable_member(o)
-                            and is_structural_type(
-                                    remove_all_extents(type_of(o)));
-                        });
-
-    }
-}
-#endif
 
 template <class T>
 struct Reflect;
