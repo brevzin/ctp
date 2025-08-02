85eb3c5b17a5690b52c65eb9a1e6404255d999ac
Barry Revzin <barry.revzin@gmail.com>
2025-08-02 06:32:49 -0500

    Fixing is_structural

--- include/ctp/core.hh
+++ include/ctp/core.hh
@@ -31,21 +31,22 @@ namespace ctp {
 //  };
 
 #ifndef CTP_META_IS_STRUCTURAL
+#include <algorithm>
 namespace std::meta {
     // this really should be in std:: but it's not yet, so...
     consteval auto is_structural_type(info type) -> bool {
-        auto ctx = std::meta::access_context::unchecked();
+        auto ctx = access_context::unchecked();
 
         return is_scalar_type(type)
             or is_lvalue_reference_type(type)
             or is_class_type(type)
-                and std::ranges::all_of(bases_of(type, ctx),
-                        [](std::meta::info o){
+                and ranges::all_of(bases_of(type, ctx),
+                        [](info o){
                             return is_public(o)
                             and is_structural_type(type_of(o));
                         })
-                and std::ranges::all_of(nonstatic_data_members_of(type, ctx),
-                        [](std::meta::info o){
+                and ranges::all_of(nonstatic_data_members_of(type, ctx),
+                        [](info o){
                             return is_public(o)
                             and not is_mutable_member(o)
                             and is_structural_type(
